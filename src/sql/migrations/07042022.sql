05efc4f4ee66d5a7a404429ddf5d9f16574c9e81
Roger Urich <roger@itsovertime.com>
2022-07-04 00:38:44 -0700

    add update to non-admins

--- src/sql/migrations/07042022.sql
+++ src/sql/migrations/07042022.sql
@@ -2,3 +2,6 @@ ALTER TABLE public.core_employee
 (
     ADD COLUMN midde_name text
 );
+
+GRANT SELECT, UPDATE ON core_employee TO EMPLOYEE_MINION;
+GRANT SELECT, UPDATE ON core_user TO EMPLOYEE_MINION;
